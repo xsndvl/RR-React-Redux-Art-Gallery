80c9b539b79aab48dba85d24becdc58ba834137d
Joswag Sandoval <sergiojr100@outlook.com>
2022-05-06 20:18:41 -0500

    best attempt

--- src/App.js
+++ src/App.js
@@ -3,8 +3,24 @@ import Nav from './components/Nav'
 import ContentWrapper from './components/ContentWrapper'
 import Footer from './components/Footer'
 
-function App() {
-  
+import {useEffect} from "react"
+
+import { useSelector, useDispatch, connect} from 'react-redux'
+const mapStateToProps = (state) => ({
+  objectId: state.data.objectId
+})
+
+const fetchAPI = "https://collectionapi.metmuseum.org/public/collection/v1/objects/[insert an object id here]"
+const fetchData = async() => {
+  const response = await fetch(fetchAPI)
+  const resData = await response.json()
+}
+fetchData()
+
+function App({store, dispatch}) {
+  useEffect(() => {
+    dispatch(fetchData())
+  }, [store.objectId, dispatch])
   return (
     <div style={{ backgroundColor: 'white', color: 'black' }} className="App">
       <Nav />
@@ -14,4 +30,4 @@ function App() {
   );
 }
 
-export default App;
+export default connect(mapStateToProps)(App)
